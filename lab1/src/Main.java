70960c9e589115998648cfb4b2ceb43f8506cfad
dimashevchukk <91614391+dimashevchukk@users.noreply.github.com>
2023-09-08 19:23:47 +0300

    Add files via upload

---
+++ lab1/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Class realizes the implementation of laboratory work №1
+ *
+ * @author Dmytro
+ * @version 1.0
+ * @since version 1.0
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Shevchuk Dmytro KI-306");
+    }
+}
